645c9aab043773da164f95bc3664b657a8d6d4fb
PradeepKK <51114651+PradeepKK13@users.noreply.github.com>
2021-01-26 10:47:04 +0400

    Update changeappicon.java

--- src/android/changeappicon.java
+++ src/android/changeappicon.java
@@ -49,7 +49,7 @@ public class changeappicon extends CordovaPlugin {
     public void ChanageToIcon(String iconname,  CallbackContext callbackContext) {
 		Log.i(TAG, "ChanageToIcon: "+ iconname);
 		//final Context ct = this.cordova.getActivity().getApplicationContext();
-		final Context ct = this.getActivity().getApplicationContext();
+		final Context ct = getApplication().getApplicationContext();
         PackageManager pm = ct.getPackageManager();
 		switch (iconname){
             case "Icon1":
